2b7e2b6ac1dcbe6c0d2895600962d9deb30519d7
hitzui <jose_miguel_1987@yahoo.com>
2018-02-28 21:28:23 -0600

    from home 28/02/2018 9:28

--- app/src/main/java/innovasoft/com/ejemplo01/activity/ListaArticulos.java
+++ app/src/main/java/innovasoft/com/ejemplo01/activity/ListaArticulos.java
@@ -1,13 +1,22 @@
 package innovasoft.com.ejemplo01.activity;
 
+import android.app.Dialog;
+import android.app.FragmentManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 import innovasoft.com.ejemplo01.R;
 import innovasoft.com.ejemplo01.adapter.ArticuloAdapter;
 import innovasoft.com.ejemplo01.converter.MyGsonHttpMessageConverter;
+import innovasoft.com.ejemplo01.dialog.DialogArticulos;
 import innovasoft.com.ejemplo01.models.Articulos;
 
 public class ListaArticulos extends AppCompatActivity {
@@ -53,30 +63,12 @@ public class ListaArticulos extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void cargarArticulos() {
-        try {
-            /*if (android.os.Build.VERSION.SDK_INT > 9) {
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-            }*/
-            final String url = "http://abrasa.com.ni/api/articulo";
-            RestTemplate restTemplate = new RestTemplate();
-            restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-            restTemplate.getMessageConverters().add(new MyGsonHttpMessageConverter());
-            listaArticulos = restTemplate.getForObject(url, Articulos[].class);
-        } catch (Exception e) {
-            Log.e("MainActivity", e.getMessage(), e);
-        }
-    }
+
     public class RequestAsyncTask extends AsyncTask<Void, Void,Articulos[]>{
 
         @Override
         protected Articulos[] doInBackground(Void... voids) {
             try {
-            /*if (android.os.Build.VERSION.SDK_INT > 9) {
-                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-                StrictMode.setThreadPolicy(policy);
-            }*/
                 final String url = "http://abrasa.com.ni/api/articulo";
                 RestTemplate restTemplate = new RestTemplate();
                 restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
@@ -93,6 +85,23 @@ public class ListaArticulos extends AppCompatActivity {
             listViewArticulos = findViewById(R.id.listViewArticulos);
             ArticuloAdapter articuloAdapter = new ArticuloAdapter(getApplicationContext(), listaArticulos);
             listViewArticulos.setAdapter(articuloAdapter);
+            listViewArticulos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    final Dialog dialog = new Dialog(ListaArticulos.this); // Context, this, etc.
+                    Articulos articulo = listaArticulos[i];
+                    String url = "http://abrasa.com.ni"+articulo.getRutaimg();
+                    dialog.setContentView(R.layout.dialog_articulo);
+                    //dialog.setTitle(R.string.aceptar_dialog);
+                    ImageView dialogImage = dialog.findViewById(R.id.imgDialogArticulo);
+                    TextView lblDescripcion = dialog.findViewById(R.id.lblDialogDescripcion);
+                    TextView lblIdArticulo = dialog.findViewById(R.id.lblDialogIdArticulo);
+                    Glide.with(view).load(url).into(dialogImage);
+                    lblDescripcion.setText(articulo.getDescripcion());
+                    lblIdArticulo.setText(articulo.getIdarticulo());
+                    dialog.show();
+                }
+            });
         }
     }
 
---
+++ app/src/main/java/innovasoft/com/ejemplo01/dialog/DialogArticulos.java
@@ -0,0 +1,36 @@
+package innovasoft.com.ejemplo01.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import innovasoft.com.ejemplo01.R;
+
+/**
+ * Created by josemiguel on 28/02/2018.
+ */
+
+public class DialogArticulos extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_articulo, null))
+                // Add action buttons
+                .setPositiveButton(R.string.aceptar_dialog, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
